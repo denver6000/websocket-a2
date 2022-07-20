1a9d9f90a815d19b6e2581bd210e0acb220ec7bb
denver01 <denverballesteros01@gmail.com>
2022-07-20 19:53:04 +0800

    CHANGED A FEW SHIT

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const {Server}= require("socket.io")
 
 let PORT = process.env.PORT || '6000'
-const io = new Server(PORT)
+const io = new Server(PORT, {
+    cors: {
+        origin: "http://192.168.88.13/"
+    }
+})
 
 console.log('Server Running....')
 
 io.on('connection', socket => {
     console.log(`${socket.id} connected`)
+    console.log(`${socket.client.request.headers} connected`)
     socket.emit('welcome-message', "You are connected")
 })
