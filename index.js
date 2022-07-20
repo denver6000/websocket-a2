457830532cb8e8f34ecc92c9463b0e03adfd6b96
denver01 <denverballesteros01@gmail.com>
2022-07-20 09:10:33 +0800

    debug statement

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const io = new Server(PORT, {
     
 })
 
+console.log('Server Running....')
+
 io.on('connection', socket => {
     console.log(`${socket.id} connected`)
     socket.emit('welcome-message', "You are connected")
