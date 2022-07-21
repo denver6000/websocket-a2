7ec135cda8809d36b784e0b3cbc593ff5cf99031
denver01 <denverballesteros01@gmail.com>
2022-07-21 12:13:48 +0800

    server http changes

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-const {Server}= require("socket.io")
+const cors = require('cors')
 
-let PORT = process.env.PORT || '6000'
-const io = new Server(PORT, {
+const express = require('express')
+const app = express()
+app.use(cors())
+const http = require('http')
+const server = http.createServer(app)
+const {Server} = require('socket.io')
+const io = new Server(server, {
     cors: {
-        origin: "http://192.168.88.13/"
+        origin: ['https://192.168.43.93', 'http://localhost']
     }
 })
 
 console.log('Server Running....')
 
 io.on('connection', socket => {
-    console.log(`${socket.id} connected`)
-    console.log(`${socket.client.request.headers} connected`)
-    socket.emit('welcome-message', "You are connected")
+    let test = server.address().address
+    console.log(`${socket.id} connected.`)
+    socket.emit('server-info', test)
+
+    socket.on("disconnect", () => {
+        console.log(`${socket.id} disconnected.`)
+    })
+})
+
+
+
+server.listen(8081, () => {
+    console.log(`  is listening on port:8081`)
 })
